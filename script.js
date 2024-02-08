1dacf74b73ebd9e691a2d02b98a5632de681d4f4
InKrnl <haruka.nanase1223@gmail.com>
2024-02-08 16:00:12 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-fetch('https://proxy.playscape.com/games/v1/groups/7137781/games?accessFilter=1&limit=10&sortOrder=Asc')
+fetch('https://games.roblox.com/v2/groups/7137781/games?accessFilter=1&limit=10&sortOrder=Asc')
   .then(response => response.json())
   .then(data => console.log(data))
   .catch(error => console.error('Error fetching data:', error));
